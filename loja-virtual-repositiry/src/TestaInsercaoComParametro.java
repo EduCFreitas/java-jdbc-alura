fc4d745ebce02a1f5323a946fd625210e672975d
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-27 16:49:40 -0300

    Aula 4: Assumindo o controle de commit

--- loja-virtual-repositiry/src/TestaInsercaoComParametro.java
+++ loja-virtual-repositiry/src/TestaInsercaoComParametro.java
@@ -7,16 +7,25 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "Mouse'";
-		String descricao = "Mouse sem fio); delete from Produto";
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
+		connection.setAutoCommit(false); //Tira a responsabilidade do JDBC de fazer os commits na tabela
 		
 		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)"
 				, Statement.RETURN_GENERATED_KEYS);
 		
+		adicionarVariavel("SmartTV", "45 polegadas", stm);
+		adicionarVariavel("Rádio", "Rádio de bateria", stm);
+
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
+		
+//		if(nome.equals("Rádio")) {
+//			throw new RuntimeException("Não foi possível adicionar o produto");
+//		}
 		
 		stm.execute();
 		
@@ -25,7 +34,6 @@ public class TestaInsercaoComParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
-
 	}
 
 }
