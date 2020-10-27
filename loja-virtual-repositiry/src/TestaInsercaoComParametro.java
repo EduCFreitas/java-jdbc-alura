7164cc121ad824863c3130852cc67a2eb3373459
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-27 17:15:30 -0300

    Aula 4: Usando o try-with-resources

--- loja-virtual-repositiry/src/TestaInsercaoComParametro.java
+++ loja-virtual-repositiry/src/TestaInsercaoComParametro.java
@@ -8,44 +8,40 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
-		connection.setAutoCommit(false); //Tira a responsabilidade do JDBC de fazer os commits na tabela
-		
-		try {
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)"
-					, Statement.RETURN_GENERATED_KEYS);
-			
-			adicionarVariavel("SmartTV", "45 polegadas", stm);
-			adicionarVariavel("Rádio", "Rádio de bateria", stm);
-			
-			connection.commit(); //Realiza o commit quando autoCommit é falso
-			
-			stm.close();
-			connection.close();
-		}catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
-		}
-		
-		
+		try(Connection connection = factory.recuperarConexao()){
+			connection.setAutoCommit(false); //Tira a responsabilidade do JDBC de fazer os commits na tabela
 
+			try (
+					PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)"
+							, Statement.RETURN_GENERATED_KEYS)
+					){
+				adicionarVariavel("SmartTV", "45 polegadas", stm);
+				adicionarVariavel("Rádio", "Rádio de bateria", stm);
+
+				connection.commit(); //Realiza o commit quando autoCommit é falso
+			}catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
+		}
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
+
 		if(nome.equals("Rádio")) {
 			throw new RuntimeException("Não foi possível adicionar o produto");
 		}
-		
+
 		stm.execute();
-		
-		ResultSet rst = stm.getGeneratedKeys();
-		while(rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: " + id);
+
+		try(ResultSet rst = stm.getGeneratedKeys()){
+			while(rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
 		}
 	}
 
