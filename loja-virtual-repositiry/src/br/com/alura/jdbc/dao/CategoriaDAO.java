48626adf34219391e8a0285c5af1e8ecfc33b256
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-28 15:36:49 -0300

    Aula 8: Implementando CategoriaController

--- loja-virtual-repositiry/src/br/com/alura/jdbc/dao/CategoriaDAO.java
+++ loja-virtual-repositiry/src/br/com/alura/jdbc/dao/CategoriaDAO.java
@@ -18,24 +18,29 @@ public class CategoriaDAO {
 		this.connection = connection;
 	}
 
-	public List<Categoria> listar() throws SQLException{
-		List<Categoria> categorias = new ArrayList<Categoria>();
+	public List<Categoria> listar(){
+		try {
+			List<Categoria> categorias = new ArrayList<Categoria>();
 
-		System.out.println("Executando a query de listar categoria");
+			System.out.println("Executando a query de listar categoria");
 
-		String sql = "SELECT ID, NOME FROM CATEGORIA";
+			String sql = "SELECT ID, NOME FROM CATEGORIA";
 
-		try(PreparedStatement pstm = connection.prepareStatement(sql)){
-			pstm.execute();
+			try(PreparedStatement pstm = connection.prepareStatement(sql)){
+				pstm.execute();
 
-			try(ResultSet rst = pstm.getResultSet()){
-				while(rst.next()) {
-					Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
-					categorias.add(categoria);
+				try(ResultSet rst = pstm.getResultSet()){
+					while(rst.next()) {
+						Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+						categorias.add(categoria);
+					}
 				}
 			}
+			return categorias;
+			
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
 		}
-		return categorias;
 	}
 
 	public List<Categoria> listarComProdutos() throws SQLException {
